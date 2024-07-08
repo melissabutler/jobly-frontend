088ba1ad4cc7d4d053d4d835e373712ab14e71e7
melissabutler <melissaebutler2015@gmail.com>
2024-07-08 15:04:25 -0400

    adjusted api confit to REACT_APP instead of VITE

--- api.js
+++ api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_APP_BASE_URL || "http://localhost:3001";
-console.log(BASE_URL)
+const BASE_URL = import.meta.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 /** API Class.
  *
